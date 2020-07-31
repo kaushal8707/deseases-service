ad3ad78646cc19f6bb589faa7ac73943cf8ebf02
kaushal8707 <kaushal.singh029@gmail.com>
2020-07-31 11:28:46 +0530

    Initial commit

---
+++ src/main/java/com/always/learner/deseasesservice/MainController.java
@@ -0,0 +1,12 @@
+package com.always.learner.deseasesservice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/diseases")
+    public String doctors(){
+        return "List of diseases";
+    }
+}
---
+++ src/test/java/com/always/learner/deseasesservice/DeseasesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.always.learner.deseasesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeseasesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
